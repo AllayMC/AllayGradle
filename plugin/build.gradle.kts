84b9bd7e67ae8e91e4b72751f8054a98c56d97ab
daoge_cmd <3523206925@qq.com>
2025-03-19 22:00:02 +0800

    build: init jitpack related stuff

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
+    `maven-publish`
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlinx.serialization)
 }
@@ -21,6 +22,54 @@ gradlePlugin {
             id = libs.plugins.allay.gradle.get().pluginId
             version = libs.versions.allay.gradle.get()
             implementationClass = "org.allaymc.gradle.AllayPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        // Jitpack requires us to publish artifacts to local maven repo
+        mavenLocal()
+    }
+
+    java {
+        withSourcesJar()
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            pom {
+                inceptionYear.set("2025")
+                packaging = "jar"
+                url.set("https://github.com/AllayMC/AllayGradle")
+
+                scm {
+                    connection.set("scm:git:git://github.com/AllayMC/AllayGradle.git")
+                    developerConnection.set("scm:git:ssh://github.com/AllayMC/AllayGradle.git")
+                    url.set("https://github.com/AllayMC/AllayGradle")
+                }
+
+                licenses {
+                    license {
+                        name.set("LGPL 3.0")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("AllayMC Team")
+                        organization.set("AllayMC")
+                        organizationUrl.set("https://github.com/AllayMC")
+                    }
+                }
+            }
         }
     }
 }
