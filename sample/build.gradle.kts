589ecfa9af49203910a6bbadc507ec58208f2df2
daoge_cmd <3523206925@qq.com>
2025-03-20 00:37:41 +0800

    fix: use absolute path for test plugin entrance

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -7,7 +7,7 @@ allay {
     version = "0.2.0"
     plugin {
         name = "TestPlugin"
-        entrance = ".TestPlugin"
+        entrance = "org.allaymc.gradle.sample.TestPlugin"
         description = "Test plugin of AllayGradle!"
         authors += "Cdm2883"
         version = libs.versions.allay.gradle
