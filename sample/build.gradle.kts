2798f36dd12aaa4601db4000fd6aad32042be3b5
Cdm2883 <Cdm2883@hotmail.com>
2025-10-25 23:41:07 +0800

    refactor: use relative entrance in test plugin descriptor

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     alias(libs.plugins.allay.gradle)
 }
 
+group = "org.allaymc.gradle.sample"  // for entrance splicing
+
 allay {
     api = "0.15.0"
     plugin {
         name = "TestPlugin"
-        entrance = "org.allaymc.gradle.sample.TestPlugin"
+        entrance = ".TestPlugin"  // "org.allaymc.gradle.sample.TestPlugin"
         description = "Test plugin of AllayGradle!"
         authors += "Cdm2883"
         version = libs.versions.allay.gradle
